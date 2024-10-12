64d94518892cfa3556f2c2b645064558c2022614
Mukulmw <mukulmw@gmail.com>
2024-10-12 20:00:12 +0200

    Added new feature letting user to continue play

--- task/main.py
+++ task/main.py
@@ -28,35 +28,38 @@ def compare(uscore, cscore):
         return "You lose, comp score higher than you!"
     else:
         return "You lose for no reason!"
+def play_game():
+    print(art.logo)
+    user_cards = []
+    comp_cards = []
+    is_game_over = False
 
-print(art.logo)
-user_cards = []
-comp_cards = []
-is_game_over = False
+    for i in range(0, 2):
+        user_cards.append(deal_card())
+        comp_cards.append(deal_card())
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(comp_cards)
+        print(f"Your cards: {user_cards}, Current score: {user_score}")
+        print(f"Computer first card: {comp_cards[0]}")
 
-for i in range(0, 2):
-    user_cards.append(deal_card())
-    comp_cards.append(deal_card())
-while not is_game_over:
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(comp_cards)
-    print(f"Your cards: {user_cards}, Current score: {user_score}")
-    print(f"Computer first card: {comp_cards[0]}")
-
-    if user_score == 0 or computer_score == 0 or user_score>21:
-        is_game_over = True
-
-    else:
-        user_should_continue = input("Do you want to draw another card? Y or N").lower()
-        if user_should_continue == "y":
-            user_cards.append(deal_card())
-        else:
+        if user_score == 0 or computer_score == 0 or user_score>21:
             is_game_over = True
-while computer_score < 17 and computer_score != 0:
-    comp_cards.append(deal_card())
-    computer_score = calculate_score(comp_cards)
 
-print(compare(user_score, computer_score))
-print(f"User Cards: {user_cards}, Computer cards: {comp_cards}")
+        else:
+            user_should_continue = input("Do you want to draw another card? Y or N").lower()
+            if user_should_continue == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+    while computer_score < 17 and computer_score != 0:
+        comp_cards.append(deal_card())
+        computer_score = calculate_score(comp_cards)
 
+    print(compare(user_score, computer_score))
+    print(f"User Cards: {user_cards}, Computer cards: {comp_cards}")
+
+while input("Do you want to play again? Y or No").lower() == "y":
+    print("\n" * 20)
+    play_game()
 
